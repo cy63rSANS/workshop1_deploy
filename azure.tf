2901553c380ed673dcef083e088d99949ee99644
Simon Vernon <svernon@sans.org>
2023-07-03 13:09:30 +0100

    Create azure.tf

---
+++ azure.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+}
