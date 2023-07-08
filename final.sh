872fd3bf7b4b9a351cff73f5e003bd375c61484d
Simon Vernon <svernon@sans.org>
2023-07-08 09:57:21 +0100

    app runtime and firewall config

--- final.sh
+++ final.sh
@@ -8,7 +8,7 @@ ip=$(az network public-ip list -g CloudAce_Workshop | jq -r .[].ipAddress)
 az vm run-command invoke -g CloudAce_Workshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/IPADDR/$ip/g' /var/www/html/index.html'
 az vm run-command invoke -g CloudAce_Workshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/ACCNAME/$storage/g' /nodeProject/search5.js'
 az vm run-command invoke -g CloudAce_Workshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's@ACCKEY@$key@g' /nodeProject/search5.js'
-az vm run-command invoke -g CloudAce_Workshop -n Webserver --command-id RunShellScript --scripts 'node /nodeProject/search5'
+az vm run-command invoke -g CloudAce_Workshop -n Webserver --command-id RunShellScript --scripts 'nohup node /nodeProject/search5.js'
 
 echo "Address of the Webserver = http://$ip"
 echo "Storage Account name = $storage"
