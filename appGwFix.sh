f95b4f1c1ec231f35dedb700c481e0aa45d72b51
Simon Vernon <svernon@sans.org>
2023-08-04 11:19:45 +0100

    clean up and api revisions

--- appGwFix.sh
+++ appGwFix.sh
@@ -5,6 +5,6 @@ ip=$(az network public-ip list -g SANSWorkshop | jq -r .[0].ipAddress)
 ip1=$(az network public-ip list -g SANSWorkshop | jq -r .[1].ipAddress)
 
 
-az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/$ip/$ip1/g' /var/www/html/index.html'
+az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/$ip/$ip1/g' /var/www/html/sript.js'
 
 echo "New Webserver Address = http://$ip1"
--- final.sh
+++ final.sh
@@ -5,9 +5,9 @@ storage=$(az storage account list -g SANSWorkshop | jq -r .[].name)
 key=$(az storage account keys list -g SANSWorkshop -n $storage | jq -r .[0].value)
 ip=$(az network public-ip list -g SANSWorkshop | jq -r .[].ipAddress)
 
-az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/IPADDR/$ip/g' /var/www/html/index.html'
-az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/ACCNAME/$storage/g' /nodeProject/search5.js'
-az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's@ACCKEY@$key@g' /nodeProject/search5.js'
+az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/IPADDR/$ip/g' /var/www/html/script.js'
+az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/ACCNAME/$storage/g' /nodeProject/api.js'
+az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's@ACCKEY@$key@g' /nodeProject/api.js'
 az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts '/nodeProject/start.sh &'
 
 echo "Webserver Address = http://$ip"
