f547828bb722ed9386881749abb3392fb4cb8331
Simon Vernon <svernon@sans.org>
2023-08-05 20:29:17 +0100

    IP address fix

--- appGwFix.sh
+++ appGwFix.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Workshop1 app gateway IP fix script - S.Vernon | SANS | CloudAce Workshop
 
-ip=$(az network public-ip list -g SANSWorkshop | jq -r .[0].ipAddress)
-ip1=$(az network public-ip list -g SANSWorkshop | jq -r .[1].ipAddress)
+ip=$(az network public-ip show -n PIP-Webserver --resource-group SANSWorkshop | jq .ipAddress -r)
+ip1=$(az network public-ip show -n appGWip --resource-group SANSWorkshop | jq .ipAddress -r)
 
 
-az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/$ip/$ip1/g' /var/www/html/sript.js'
+az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/$ip/$ip1/g' /var/www/html/script.js'
+az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/$ip/$ip1/g' /var/www/html/index.html'
 
 echo "New Webserver Address = http://$ip1"
