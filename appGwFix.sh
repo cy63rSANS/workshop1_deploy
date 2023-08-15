6aa66ebc888d755cbc0aaeeebdf196459f0e4cde
Simon Vernon <svernon@sans.org>
2023-08-15 22:39:30 +0100

    Update appGwFix.sh

--- appGwFix.sh
+++ appGwFix.sh
@@ -2,7 +2,7 @@
 # Workshop1 app gateway IP fix script - S.Vernon | SANS | CloudAce Workshop
 
 ip=$(az network public-ip show -n PIP-Webserver --resource-group SANSWorkshop | jq .ipAddress -r)
-ip1=$(az network public-ip show -n appGWip --resource-group SANSWorkshop | jq .ipAddress -r)
+ip1=$(az network public-ip show -n appGWpip --resource-group SANSWorkshop | jq .ipAddress -r)
 
 
 az vm run-command invoke -g SANSWorkshop -n Webserver --command-id RunShellScript --scripts 'sed -i 's/$ip/$ip1/g' /var/www/html/script.js'
